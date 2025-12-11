e469d435fc70f5d85ec119ccf5551e5dee313905
Eddie Taliaferro <eddielacrosse2@gmail.com>
2025-12-11 14:12:34 -0500

    Deleted Vitest

--- f-e/src/tests/landing-page.test.ts
+++ f-e/src/tests/landing-page.test.ts
@@ -1,7 +1,2 @@
-import { describe, it, expect } from 'vitest';
-import { render } from '@testing-library/svelte';
-import LandingPage from '../routes/+page.svelte';
-
-describe('Landing Page', () => {
- 
-});
+// import { render } from '@testing-library/svelte';
+// import LandingPage from '../routes/+page.svelte';
--- f-e/vite.config.ts
+++ f-e/vite.config.ts
@@ -1,33 +1,8 @@
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vitest/config';
-import { playwright } from '@vitest/browser-playwright';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	test: {
-		expect: { requireAssertions: true },
-		projects: [
-			{
-				test: {
-					name: 'client',
-					browser: {
-						enabled: true,
-						provider: playwright(),
-						instances: [{ browser: 'chromium', headless: true }]
-					},
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**']
-				}
-			},
-			{
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
-				}
-			}
-		]
-	}
+	plugins: [tailwindcss(), sveltekit()]
 });
+
