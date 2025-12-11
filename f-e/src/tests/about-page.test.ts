557f1f266ffa9bd9700e506acb4bf4580399fae5
Eddie Taliaferro <eddielacrosse2@gmail.com>
2025-12-11 13:43:02 -0500

    Delete About Route

--- f-e/src/tests/about-page.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import { render } from '@testing-library/svelte';
-import AboutPage from '../routes/about/+page.svelte';
-
-describe('About Page', () => {
-  
-});
